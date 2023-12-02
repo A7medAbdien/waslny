863bf3f8ba4a0ca1a77b0d91f2a81adfa2328dec
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-12-02 16:36:32 +0300

    test

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Manrope, Noto_Serif, Roboto, Roboto_Flex } from "next/font/google";
+import { Manrope, Roboto_Flex } from "next/font/google";
 
 export const headerFont = Roboto_Flex({ subsets: ["latin"] })
 export const contentFont = Manrope({ subsets: ['latin'] })
